a9b416ce9331519fcd7d50287fefad6754e80d5c
Alexander Minges <alexander.minges@gmail.com>
2019-12-01 16:45:26 +0100

    mqtt: Connect to broker immediately and use internal loop to publish messages

--- klippy/extras/mqtt.py
+++ klippy/extras/mqtt.py
@@ -17,7 +17,7 @@ class PrinterMqtt(object):
         broker_url = config.get('broker_url', '127.0.0.1')
         broker_port = config.getint('broker_port', 1883)
         broker_keepalive = config.getint('broker_keepalive', 60)
-        client_id = config.get('client_id', uuid.uuid4()) # use random UUID by default as client ID
+        client_id = config.get('client_id', str(uuid.uuid4())) # use random UUID by default as client ID
         protocol_version = config.getchoice('protocol', self.protocols, 'mqttv31')
         self.topic_base = config.get('topic', 'klipper') # base topic to publish messages
 
@@ -31,7 +31,8 @@ class PrinterMqtt(object):
             raise config.error("Protocol version has to be one of 'mqttv31', 'mqttv311' or 'mqttv5'")
 
         self.client = mqtt.Client(client_id, protocol)
-        self.client.connect_async(broker_url, port=broker_port, keepalive=broker_keepalive)
+        self.client.connect(broker_url, port=broker_port, keepalive=broker_keepalive)
+        self.client.loop_start()
 
         self.printer.register_event_handler('klippy:connect', self.handle_connect)
         self.printer.register_event_handler('klippy:disconnect', self.handle_disconnect)
@@ -44,11 +45,14 @@ class PrinterMqtt(object):
         self.check_timer = reactor.register_timer(self.update, reactor.NOW)
 
         messages = [{'topic': 'klippy/status', 'payload': 'connecting'}]
-        self.publish(messages, run_loop=True)
+        self.publish(messages)
 
     def handle_disconnect(self):
         messages = [{'topic': 'klippy/status', 'payload': 'disconnected'}]
-        self.publish(messages, run_loop=True)
+        self.publish(messages)
+
+        self.client.loop_stop()
+        self.client.disconnect()
 
         reactor = self.printer.get_reactor()
         # kill connection to broker and stop publishing updates
@@ -56,23 +60,19 @@ class PrinterMqtt(object):
 
     def handle_shutdown(self):
         messages = [{'topic': 'klippy/status', 'payload': 'shutdown'}]
-        self.publish(messages, run_loop=True)
+        self.publish(messages)
 
     def handle_ready(self):
         messages = [{'topic': 'klippy/status', 'payload': 'ready'}]
-        self.publish(messages, run_loop=True)
+        self.publish(messages)
 
     def update(self, eventtime):
-        # send queued messages and handle network traffic to broker
-        self.client.loop()
+        
 
-    def publish(self, messages, run_loop=False):
+    def publish(self, messages):
     # Publish messages to mqtt broker. Accepts list of dictionaries with keys 'topic' and 'payload'
-    # Setting 'run_loop' to 'True' will publish messages immediately instead of waiting for the next event loop. 
         for message in messages:
             self.client.publish('{}/{}'.format(self.topic_base, message['topic']), message['payload'])
-        if run_loop:
-            self.client.loop()
 
 def load_config(config):
     return PrinterMqtt(config)
